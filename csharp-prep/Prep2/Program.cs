97f283e7b6c92f9618feae366b40eeed4dbffc88
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 12:43:09 -0600

    Created a system to turn grade percentage into a letter grade with + and - letter grades included, and define to the user if they passed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter Grade Percentage...");
+        float Grade = float.Parse(Console.ReadLine());
+        string LetterGrade = "F";
+        if (Grade >= 90)
+        {
+            LetterGrade = "A";
+        }
+        else if (Grade >= 80)
+        {
+            LetterGrade = "B";
+        }
+        else if (Grade >= 70)
+        {
+            LetterGrade = "C";
+        }
+        else if (Grade >= 60)
+        {
+            LetterGrade = "D";
+        }
+        if (LetterGrade != "F")
+        {
+            float GradeLevel = Grade % 10;
+            if (GradeLevel >= 7 && LetterGrade != "A")
+            {
+                LetterGrade += '+';
+            }
+            else if (GradeLevel < 3 && Grade < 100)
+            {
+                LetterGrade += '-';
+            }
+        }
+        Console.WriteLine($"You got a {LetterGrade}");
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Yay you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up you will get it next time");
+        }
     }
 }
