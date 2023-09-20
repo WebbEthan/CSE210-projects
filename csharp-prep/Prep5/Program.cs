abe93dbcbf7e638c2cedc0ff36bee56d1eb65520
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 21:22:03 -0600

    Made multiple functions as defined in the assinment that do various tasks

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int FavoriteNumber = PromptUserNumber();
+        DisplayResult(name, SquareNumber(FavoriteNumber));
+    }
+    private static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    private static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        return Console.ReadLine();
+    }
+    private static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        return int.Parse(Console.ReadLine());
+    }
+    private static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    private static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
     }
 }
