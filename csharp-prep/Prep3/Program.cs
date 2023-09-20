6e804eaea6b4a42a0e0df4e9fdc1fa9c78cc34d5
BuildTools <unconfigured@null.spigotmc.org>
2023-09-19 19:10:16 -0600

    Created number guessing game that you can play as many times as you want. Added color for fun.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
+    // Thought it looked boring so I added colors.
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        while (true)
+        {
+            // Sets up round
+            int guesses = 0;
+            int RandomNumber = random.Next(1, 100);
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("What is your guess?");
+                // Ensures that the input is a number and parses into the guess variable
+                Console.ForegroundColor = ConsoleColor.Blue;
+                if (int.TryParse(Console.ReadLine(), out int guess))
+                {
+                    guesses++;
+                    if (guess > RandomNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Lower");
+                    }
+                    else if (guess < RandomNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Higher");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please input a number from 1 to 100");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"It only took you {guesses} guesses!");
+            // Prompts user to play again
+            Console.WriteLine("Do you want to play again?");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            if (Console.ReadLine() != "yes")
+            {
+                return;
+            }
+        }
     }
 }
